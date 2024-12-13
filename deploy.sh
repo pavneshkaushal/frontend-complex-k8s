583bd01766f17e0a12279100702e6d0a28d0483e
PavneshKaushal <pavnesh.kaushal@gmail.com>
2024-12-12 22:50:59 -0500

    fixed deploy sh file

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@ docker build -t panak4572/frontend-complex-k8s-worker:latest -t panak4572/fronte
 docker push panak4572/frontend-complex-k8s-client:latest
 docker push panak4572/frontend-complex-k8s-server:latest
 docker push panak4572/frontend-complex-k8s-worker:latest
+
 docker push panak4572/frontend-complex-k8s-client:$SHA
 docker push panak4572/frontend-complex-k8s-server:$SHA
 docker push panak4572/frontend-complex-k8s-worker:$SHA
 
-kuberctl apply -f k8s
-
-kubectl set image deployments/client-deployments/client client=panak4572/frontend-complex-k8s-client:$SHA
-kubectl set image deployments/server-deployments/server server=panak4572/frontend-complex-k8s-server:$SHA
-kubectl set image deployments/worker-deployments/worker worker=panak4572/frontend-complex-k8s-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=panak4572/multi-server-k8s:$SHA
+kubectl set image deployments/client-deployment client=panak4572/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=panak4572/multi-worker-k8s:$SHA
